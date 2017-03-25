f02f681a7908c3826b638f609c2066f1f101d619
GJDavila <gjdavilaguillen@gmail.com>
2017-03-24 21:56:21 -0500

    subiendo algoritmo

--- multiplicacion matrices/main.c
+++ multiplicacion matrices/main.c
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <stdio.h>
 using namespace std;
- int main()
+
+int main( )
 {
-
-	cout<<"hola mundo"<<endl;
-
+  int af, ac, bf, bc, i, j, k;
+  
+  int A[af][ac], B[bf][bc], C[af][bc]; 
+   
+ 
+  for(i=0;i<af;i++){
+      for(j=0;j<bc;j++){
+          C[i][j]=0;
+          for(k=0;k<ac;k++){
+              C[i][j]=(C[i][j]+(A[i][k]*B[k][j]));
+          }
+      }
+  }
+  
 }
